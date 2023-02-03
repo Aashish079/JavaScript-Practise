9e697037b6ae7ad3557bc938d300fa0e3ba1ed17
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2023-02-03 22:26:19 +0545

    callbackfunctions

--- DOM-manipulation.js
+++ DOM-manipulation.js
@@ -1,27 +1,31 @@
+//Adding strings to a HTML
+let body = document.body;
+body.append("Hello World");
+body.appendChild("Hello World");
 // document.createElement('h1');
 // h1.innerText = "Hello World! CM Punk is the best in the World!";
 
-// h1.setAttribute('id', 'heading');
-// h1.setAttribute('class', 'header');
+// // h1.setAttribute('id', 'heading');
+// // h1.setAttribute('class', 'header');
 
-//Input from User
+// //Input from User
 
-let answer = prompt('What is your name?');
+// let answer = prompt('What is your name?');
 
-console.log(`Your name is ${answer}`);
+// console.log(`Your name is ${answer}`);
 
-//Events Listner
+// //Events Listner
 
-let h1 = document.createElement('h1');
-h1.innerText = "Type into the input to make this text change"
+// let h1 = document.createElement('h1');
+// h1.innerText = "Type into the input to make this text change"
 
-let input = document.createElement('input');
-input.setAttribute('type', 'text');
+// let input = document.createElement('input');
+// input.setAttribute('type', 'text');
 
-document.body.innerText =''
-document.body.appendChild(h1);
-document.body.appendChild(input);
+// document.body.innerText =''
+// document.body.appendChild(h1);
+// document.body.appendChild(input);
 
-input.addEventListener('change', function(){
-    h1.innerText = input.value;
-})
+// input.addEventListener('change', function(){
+//     h1.innerText = input.value;
+// })
